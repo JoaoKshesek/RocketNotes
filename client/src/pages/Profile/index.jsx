6758cd5316a59ede35ba8fa9a97117d933dca290
JoaoKshesek <kshesek.joaovalter@gmail.com>
2023-06-14 23:58:39 -0300

    feat: upload client

---
+++ client/src/pages/Profile/index.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
+import avatarPlaceHolder from "../../assets/avatar_placeholder.svg";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Avatar, Form } from "./styles";
+
+export function Profile() {
+  const { user, updateProfile } = useAuth();
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [oldPassword, setOldPassword] = useState();
+  const [newPassword, setNewPassword] = useState();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceHolder;
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
+  async function handleUpdateProfile() {
+    const updated = {
+      name,
+      email,
+      newPassword: newPassword,
+      old_password: oldPassword,
+    };
+    const userUpdated = Object.assign(user, updated);
+    await updateProfile({ user: userUpdated, avatarFile });
+  }
+
+  async function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handleBack}>
+          <FiArrowLeft />
+        </button>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src={avatar} alt="Foto do usuário" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
+          </label>
+        </Avatar>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setOldPassword(e.target.value)}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+
+        <Button title="Salvar" onClick={handleUpdateProfile} />
+      </Form>
+    </Container>
+  );
+}
